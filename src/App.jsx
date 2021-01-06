563b76e3100b451d700eee4f346a73c6b6391626
Keith Shep <kmson7@gmail.com>
2021-01-06 16:38:37 +0800

    save before implementing infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 // import { createApi } from 'unsplash-js';
 import './App.css';
 import PostList from './PostList'
@@ -6,26 +7,36 @@ import Navbar from './Navbar'
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const [query, setQuery] = useState('hi');
+  const [page, setPage] = useState('1');
+  // const [dataLength, setDataLength] = useState(0);
 
   const accessKeyUnsplash = '6c446b49b72a4c559d9b9d67183d5c1de1981d16f309063c3b994086e6ce1a26';
   const baseUrlUnsplash = 'https://api.unsplash.com';
-  let query = 'london'
-  const url = `${baseUrlUnsplash}/search/photos?query=${query}&client_id=${accessKeyUnsplash}`
-
+  const url = `${baseUrlUnsplash}/search/photos?query=${query}&page=${page}&client_id=${accessKeyUnsplash}`
 
   useEffect(() => {
-    fetch(url, { headers : { 'Content-Type': 'application/json','Accept': 'application/json' } })
+    fetch(url, { headers :
+      { 'Content-Type': 'application/json',
+        'Accept': 'application/json' }
+      })
       .then(response => response.json())
-      .then(data => setItems(data.results))
+      .then(data => {
+        console.log(data)
+        setItems(data.results)
+        // setDataLength(data.total)
+        // setPage(page+1)
+      })
   }, [])
 
   return (
-    <div>
+    <>
       <Navbar />
       <div className="container w-25">
         <PostList items={items} />
       </div>
-    </div>
+
+    </>
   );
 }
 
