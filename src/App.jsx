38f84158899ce65af82de1a32b1b7b408078f833
Keith Shep <kmson7@gmail.com>
2021-01-06 18:41:55 +0800

    infinite works

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,63 @@ import PostList from './PostList'
 import Navbar from './Navbar'
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [query, setQuery] = useState('hi');
-  const [page, setPage] = useState('1');
-  // const [dataLength, setDataLength] = useState(0);
-
   const accessKeyUnsplash = '6c446b49b72a4c559d9b9d67183d5c1de1981d16f309063c3b994086e6ce1a26';
   const baseUrlUnsplash = 'https://api.unsplash.com';
-  const url = `${baseUrlUnsplash}/search/photos?query=${query}&page=${page}&client_id=${accessKeyUnsplash}`
 
+  const [dataLength, setDataLength] = useState(0);
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState('london');
+  const [page, setPage] = useState(50);
+
+  // Init first 10 posts
   useEffect(() => {
+    setPage(page+1)
+    const url = `${baseUrlUnsplash}/search/photos?query=${query}&page=${page}&client_id=${accessKeyUnsplash}`
+
     fetch(url, { headers :
       { 'Content-Type': 'application/json',
         'Accept': 'application/json' }
       })
       .then(response => response.json())
       .then(data => {
-        console.log(data)
+        console.log('init data')
         setItems(data.results)
-        // setDataLength(data.total)
-        // setPage(page+1)
+        setDataLength(data.results.length)
       })
   }, [])
 
-  return (
-    <>
-      <Navbar />
-      <div className="container w-25">
-        <PostList items={items} />
-      </div>
+  // Init more posts after the initial 10
+  const loadMoreData = () => {
+    setPage(page+1)
+    const url = `${baseUrlUnsplash}/search/photos?query=${query}&page=${page}&client_id=${accessKeyUnsplash}`
+    console.log(url)
 
-    </>
+    fetch(url, { headers :
+      { 'Content-Type': 'application/json',
+        'Accept': 'application/json' }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('loading next 10 posts...')
+        setDataLength(dataLength + data.results.length)
+        setItems(items.concat(data.results))
+      })
+  }
+
+  return (
+    <React.Fragment>
+      <Navbar />
+        <div className="container w-25">
+          <InfiniteScroll
+            dataLength={dataLength}
+            next={loadMoreData}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            >
+              <PostList items={items} />
+          </InfiniteScroll>
+        </div>
+    </React.Fragment>
   );
 }
 
