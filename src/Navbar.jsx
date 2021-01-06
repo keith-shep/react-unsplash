8a46eb35dd27a226e2efb8a3b6eb125cd675e236
Keith Shep <kmson7@gmail.com>
2021-01-06 18:53:17 +0800

    remove search

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,10 +4,6 @@ const Navbar = () => {
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <span className="navbar-brand">Resplash</span>
-        <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </nav>
     )
